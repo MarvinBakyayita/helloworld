0114bb593b8a87cd16dbcd46ebd1557d1f524ba6
Marvin <bakyayitamarvin@gmail.com>
2023-01-03 00:13:46 +0300

    added exclamation marks on hello world

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 #this is my first python code 
-print("hello world")
-name=input("whats your name")
+print("hello world!!")
+name=input("whats your name ")
 #here I'm trying to collect some data from the user through the name variable
 print("Hello there "+name)
---
+++ mob.py
@@ -0,0 +1,2 @@
+#this program is for my git testing.
+print("This is a test for git. Thank you.")
