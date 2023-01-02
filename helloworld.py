1be76a493b828b9177c50fd2690d33609cc11a95
Marvin <bakyayitamarvin@gmail.com>
2023-01-02 18:19:00 +0300

    This is my first commit

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+#this is my first python code 
+print("hello world")
+name=input("whats your name")
+#here I'm trying to collect some data from the user through the name variable
+print("Hello there "+name)
