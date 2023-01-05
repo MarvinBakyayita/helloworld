83eb1b683fe07991374aa064f75208c4bf9d01bb
Marvin <bakyayitamarvin@gmail.com>
2023-01-05 23:39:47 +0300

    here I was trying to ]practice all I had learnt in one program

---
+++ learn.py
@@ -0,0 +1,17 @@
+#trying to practice what I've learnt
+
+def main():
+    x = 8
+    y = 2
+    name = input("What's your name patner? ")
+    food = input("What's your favourite food? ")
+    print("Hello there,", name.strip().title())
+    print("Your favourite food is", food.strip())
+    print( x + y )
+    print( round((x / y), 3 ))
+    print( x * y )
+    print( x - y )
+
+
+main()
+
