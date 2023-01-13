af844e5d2de30a69019b8b1be808614d2f831496
Marvin <bakyayitamarvin@gmail.com>
2023-01-13 22:20:35 +0300

    edited the programm output so as to add some statements

--- learn.py
+++ learn.py
@@ -1,12 +1,13 @@
 #trying to practice what I've learnt
 
 def main():
-    x = int(input())
-    y = int(input())
+    x = int(input("Enter x "))
+    y = int(input("Enter y "))
     name = input("What's your name patner? ")
     food = input("What's your favourite food? ")
     print("Hello there,", name.strip().title())
     print("Your favourite food is", food.strip())
+    print("your calculations are as follows")
     print( x + y )
     print( round((x / y), 3 ))
     print( x * y )
